184ce332fbce07e3f972c3e816480a01b1ea3068
Jonas Jensen <jonas.jensen@msn.com>
2016-02-06 21:29:00 +0100

    Rendering tenants as React components!

--- public/components/tenant.js
+++ public/components/tenant.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class Tenant extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {content: ''}
-    }
-
-    componentWillMount() {
-        fetch(this.props.resourceUrl)
-            .then(res => res.json())
-            .then(res => this.setState({
-                content: JSON.stringify(res)
-            }));
-    }
-
-    render() {
-        return <div>{this.state.content}</div>;
-    }
-}
-
-ReactDOM.render((<Tenant resourceUrl="/tenants/56b5cc012e5fd69c04593136"/>), document.getElementById('root'));
+export default ({name_first, name_middle, name_last, email, phone}) =>
+    <table>
+        <tbody>
+        <tr>
+            <td>Navn</td>
+            <td>{name_first} {name_middle} {name_last}</td>
+        </tr>
+        <tr>
+            <td>E-post</td>
+            <td>{email}</td>
+        </tr>
+        <tr>
+            <td>Telefon</td>
+            <td>{phone}</td>
+        </tr>
+        </tbody>
+    </table>;
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-import Tenant from '../components/tenant';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Tenants from '../components/tenant';
+
+fetch('/tenants/')
+    .then(res => res.json())
+    .then(res => ReactDOM.render((<Tenants {...res[4]} />), document.getElementById('root')));
+
