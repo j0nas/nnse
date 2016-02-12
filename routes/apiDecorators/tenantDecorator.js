7494e961eb93ced9fe839eac396d758f5144198a
Jonas Jensen <jonas.jensen@msn.com>
2016-02-12 17:38:55 +0100

    made HTTP DELETE tenants/:id/mailbox reflect changes in mailbox model

--- routes/apiDecorators/tenantDecorator.js
+++ routes/apiDecorators/tenantDecorator.js
@@ -18,28 +18,28 @@ module.exports = function (router, tenantModel) {
                 tenant._mailbox.save((saveErr) => {
                     if (saveErr) next(saveErr);
                     res.json(tenant);
-                    console.log('Updated tenant!');
                 });
             });
     });
 
+    // TODO change PUT ect to use /:id2 instead of body.id
     router.delete('/:id/mailbox', (req, res, next) => {
-        tenantModel.findByIdAndUpdate(
-            req.params.id,
-            {_mailbox: null},
-            {safe: true, new: true},
-            (err, tenant) => {
-                if (err) return next(err);
+        tenantModel
+            .findById(req.params.id)
+            .populate('_mailbox')
+            .exec((tenantFetchError, tenant) => {
+                if (tenantFetchError) return next(tenantFetchError);
+                var arr = tenant._mailbox.tenants;
+                const index = arr.indexOf(req.params.id);
+                arr.splice(index, 1);
 
-                var options = {
-                    host: request.headers.host,
-                    port: app.env.port || 3000,
-                    path: '/mail',
-                    method: 'DELETE'
-                };
-                require('http').request(options, (res) => {
-                    console.log(res);
-                    res.json(tenant);
+                tenant._mailbox.save((mailboxUpdateError) => {
+                    if (mailboxUpdateError) return next(mailboxUpdateError);
+                    tenant._mailbox = null;
+                    tenant.save((tenantUpdateError) => {
+                        if (tenantUpdateError) return next(tenantUpdateError);
+                        res.json(tenant);
+                    });
                 });
             });
     });
