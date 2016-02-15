b7f5543e7ecbb75a8cc479052d46433b5ad137b6
Jonas Jensen <jonas.jensen@msn.com>
2016-02-15 08:07:14 +0100

    Updated mongodb+mongoose deps

--- public/js/main.js
+++ public/js/main.js
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom';
 
 import ButtonGroup from '../components/button/buttonGroup';
 
-var links = [{
-    ref: '/tenants',
-    text: 'Leietakere'
-}, {
-    ref: '/mailboxes',
-    text: 'Postbokser'
-}];
+var links = [
+    {ref: '/tenants', text: 'Leietakere'},
+    {ref: '/mailboxes', text: 'Postbokser'}
+];
+
 ReactDOM.render((<ButtonGroup data={links}/>), document.getElementById('root'));
-/*
- fetch('/api/tenants/')
- .then(res => res.json())
- .then(res => res.forEach(tenant =>
- ReactDOM.render((<Tenant {...tenant} />), document.getElementById('root'))
- ));
- */
