239a110de8953e589148aa6a9984042533665136
Jonas Jensen <jonas.jensen@msn.com>
2016-03-04 21:38:41 +0100

    Remove unnecessary bloat from app.js

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var favicon = require('express-favicon');
-
-var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+const express = require('express');
+const path = require('path');
+const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public/img/favicon.ico')));
-app.set('json spaces', 2);
 
-var mongoose = require('./db/db');
+const mongoose = require('./db/db');
 mongoose.init('mongodb://127.0.0.1:27017/nnse');
 
-var entityManager = require('./routes/entityManager');
+const entityManager = require('./routes/entityManager');
 entityManager.init(app, 'api');
 entityManager.setupEntities('Tenant', 'Mailbox', 'Lease');
 
-app.use((req, res, next) => {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+const port = 3000;
+app.listen(port, err => {
+    if (err) throw new Error(err);
+    console.log('Listening on http://localhost:' + port);
 });
-
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    var errs = {
-        message: err.message,
-        error: app.get('env') === 'development' ? err : {}
-    };
-    console.log(JSON.stringify(errs));
-    next(err);
-});
-
-module.exports = app;
