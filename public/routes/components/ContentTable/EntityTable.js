4a87ebe3faf220133781ab60d6f8c40b8428b928
Jonas Jensen <jonas.jensen@msn.com>
2016-06-15 14:22:53 +0200

    [WIP] formatting dates correctly, working on populating associated entities on fetching from db

--- public/routes/components/ContentTable/EntityTable.js
+++ public/routes/components/ContentTable/EntityTable.js
@@ -1,17 +1,10 @@
 import React, {PropTypes} from "react";
 import {Link} from "react-router";
+import FormEntities from "../../EntityForm/FormEntities";
 
 export default class EntityTable extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tableHeader: [],
-            entities: []
-        };
-    }
-
     filterTableValues(val) {
-        var filter = {
+        const filter = {
             _id: false,
             __v: true
         };
@@ -19,20 +12,88 @@ export default class EntityTable extends React.Component {
         return filter[val];
     }
 
-    getTableHeaders(prop) {
-        if (!this.filterTableValues(prop)) {
-            return <th key={prop}>{prop}</th>;
-        }
+
+    getTableHeader() {
+        const entityObject = FormEntities.getEntityObject(this.props.apipath);
+        return (
+            <thead>
+            <tr>
+                <th/>
+                {Object.keys(entityObject).map(key =>
+                    <th key={entityObject[key].value}>{entityObject[key].value}</th>)}
+            </tr>
+            </thead>
+        );
+    }
+
+    getTableRow(entity) {
+        return (
+            <tr key={entity._id}>
+                {Object.keys(entity).map(key => this.getTableContent(key, entity[key], entity))}
+            </tr>
+        );
     }
 
     getTableContent(key, value) {
         if (key === '_id') {
-            return <td key={value}><Link to={this.props.apipath + '/' + value}>{value}</Link></td>;
+            return (
+                <td key={value}>
+                    <Link to={this.props.apipath + '/' + value}>{value}</Link>
+                </td>
+            );
         }
 
         if (!this.filterTableValues(key)) {
             return <td key={key + "_" + value}>{value}</td>;
         }
+    }
+
+    // TODO populate when fetching!
+
+    // TODO make date formatting less hackish -- look at input type?
+    formatDate(timestamp) {
+        if (!timestamp) {
+            return;
+        }
+
+        const stamp = timestamp.split("T")[0];
+        const split = stamp.split("-");
+        return `${split[2]}.${split[1]}.${split[0]}`;
+    }
+
+    formatEntities(entities) {
+        const entityObject = FormEntities.getEntityObject(this.props.apipath);
+        const result = [];
+        let index = 0;
+
+        entities.map(entity => {
+            result[index] = {_id: entity._id};
+
+            Object.keys(entityObject).map(key => {
+                result[index][key] = entityObject[key].type === "date" ?
+                    this.formatDate(entity[key]) : entity[key];
+
+                // if (entityObject[key].type === "entity_reference") {
+                //     fetch("/api" + entityObject[key].endpoint)
+                //         .then(res => res.json())
+                //         .then(res => res.map(fetchedEntity => {
+                //             if (fetchedEntity._id === entity._id) {
+                //                 // result[index][key] =
+                //             }
+                //         }))
+                // }
+            });
+
+            index++;
+        });
+
+        return result;
+    }
+
+
+    getTableContents() {
+        const formattedEntities = this.formatEntities(this.props.entities);
+        return <tbody>{formattedEntities.map(entity => this.getTableRow(entity))}</tbody>;
     }
 
     render() {
@@ -52,26 +113,11 @@ export default class EntityTable extends React.Component {
                 </div>
                 <div className="carEvaluationInfo">
                     <table className="table">
-                        <thead>
-                        <tr>
-                            {this.props.entities[0] &&
-                            Object.keys(this.props.entities[0]).map(prop => this.getTableHeaders(prop))}
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.props.entities.map(entity => this.getTableRow(entity))}
-                        </tbody>
+                        {this.getTableHeader()}
+                        {this.getTableContents()}
                     </table>
                 </div>
             </div>
-        );
-    }
-
-    getTableRow(entity) {
-        return (
-            <tr key={entity._id}>
-                {Object.keys(entity).map(key => this.getTableContent(key, entity[key]))}
-            </tr>
         );
     }
 }
