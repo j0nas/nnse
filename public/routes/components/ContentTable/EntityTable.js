1ac3df9d4f161db3e8b0b40cf145d8410f02b996
Jonas Jensen <jonas.jensen@msn.com>
2016-06-17 05:22:39 +0200

    implemented table search functionality

--- public/routes/components/ContentTable/EntityTable.js
+++ public/routes/components/ContentTable/EntityTable.js
@@ -11,6 +11,7 @@ export default class EntityTable extends React.Component {
         super();
         this.lastCellSorted = -1;
         this.prevPath = "none";
+        this.onSearchChange = this.onSearchChange.bind(this);
     }
 
     handleArrowCharDisplaying(cells, column, reverse) {
@@ -54,7 +55,7 @@ export default class EntityTable extends React.Component {
 
         const ref = this;
         while (--i >= 0) {
-            (function(i) {
+            (function (i) {
                 let dir = 1;
                 tableHead[i].addEventListener('click', () => ref.sortTable(table, i, (dir = 1 - dir)));
             }(i));
@@ -151,6 +152,40 @@ export default class EntityTable extends React.Component {
             this.lastCellSorted = -1;
             const entityTable = document.getElementById("entityTable");
             this.makeSortable(entityTable);
+
+            const searchInput = document.getElementById("filterTableSearch");
+            searchInput.value = "";
+        }
+    }
+
+
+    onSearchChange() {
+        const searchInput = document.getElementById("filterTableSearch");
+        const searchText = searchInput.value.toLowerCase();
+
+        const entityTable = document.getElementById("entityTable");
+        const rows = entityTable.tBodies[0].rows;
+
+        if (searchText === "") {
+            for (let i = 0; i < rows.length; i++) {
+                rows[i].style.display = "table-row";
+            }
+
+            return;
+        }
+
+        for (let i = 0; i < rows.length; i++) {
+            const cellCount = rows[i].cells.length;
+            for (let j = 0; j < cellCount; j++) {
+                if (rows[i].cells[j].textContent.toLowerCase().indexOf(searchText) > -1) {
+                    rows[i].style.display = "table-row";
+                    break;
+                }
+
+                if (j === cellCount - 1) {
+                    rows[i].style.display = "none";
+                }
+            }
         }
     }
 
@@ -163,8 +198,18 @@ export default class EntityTable extends React.Component {
                             <div className="col-xs-6">
                                 {this.props.title}
                             </div>
-                            <div className="col-xs-6 text-right">
-                                <Link className="btn btn-primary" to={this.props.apipath + '/new'}>Opprett ny</Link>
+                            <div className="col-xs-6">
+                                <div className="row text-right">
+                                    <div className="col-xs-offset-6 col-xs-4">
+                                        <input type="search" placeholder="Søk" className="form-control"
+                                               id="filterTableSearch" onChange={() => this.onSearchChange()}/>
+                                    </div>
+                                    <div className="col-xs-2">
+                                        <Link className="btn btn-primary form-control" to={this.props.apipath + '/new'}>
+                                            Opprett ny
+                                        </Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
