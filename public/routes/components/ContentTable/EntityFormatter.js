e8e62737c654f51b9fa030ebf9529270913b9bcd
Jonas Jensen <jonas.jensen@msn.com>
2016-06-20 21:48:24 +0200

    extracted entity formatting logic into separate EntityFormatter class

---
+++ public/routes/components/ContentTable/EntityFormatter.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {Link} from "react-router";
+
+export default class EntityFormatter {
+    static formatDate(timestamp) {
+        const date = new Date(timestamp);
+        return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+    }
+
+    static createForeignEntityLink(identifierFields, endpoint, entity) {
+        const sanity = identifierFields && endpoint && entity;
+        if (!sanity) {
+            return;
+        }
+
+        return <Link to={endpoint + "/" + entity._id}>{identifierFields.map(field => entity[field] + " ")}</Link>;
+    }
+
+    static processTypedEntity(entity, entityObject) {
+        switch (entityObject.type) {
+        case "date":
+            return this.formatDate(entity);
+        case "entity_reference":
+            return this.createForeignEntityLink(entityObject.identifiers, entityObject.endpoint, entity);
+        default:
+            return entity;
+        }
+    }
+
+    static formatEntities(entities, entityObject) {
+        const result = [];
+
+        entities.forEach((entity, index) => {
+            result[index] = {_id: entity._id};
+            Object.keys(entityObject).forEach(key => {
+                result[index][key] = this.processTypedEntity(entity[key], entityObject[key]);
+            });
+        });
+
+        return result;
+    }
+}
--- public/routes/components/ContentTable/EntityTable.js
+++ public/routes/components/ContentTable/EntityTable.js
@@ -2,6 +2,7 @@ import React, {PropTypes} from "react";
 import {Link} from "react-router";
 import FormEntities from "../../EntityForm/FormEntities";
 import TableSearchInput from "./TableSearchInput";
+import EntityFormatter from "./EntityFormatter";
 
 export default class EntityTable extends React.Component {
     constructor() {
@@ -30,29 +31,22 @@ export default class EntityTable extends React.Component {
 
         let tableRow = Array.prototype.slice.call(tableBody.rows, 0);
         reverse = -(Number(reverse) || -1);
-        tableRow = tableRow.sort((a, b) =>
-            reverse * (a.cells[column].textContent.trim().localeCompare(b.cells[column].textContent.trim()))
-        );
-
-        for (let i = 0; i < tableRow.length; ++i) {
-            tableBody.appendChild(tableRow[i]);
-        }
+        tableRow.sort(
+            (a, b) => reverse * (a.cells[column].textContent.trim().localeCompare(b.cells[column].textContent.trim())))
+            .forEach(row => tableBody.appendChild(row));
     }
 
     makeSortable(table) {
-        const tableHead = table.tHead && table.tHead.rows[0] && table.tHead.rows[0].cells;
+        const tableHead = table && table.tHead && table.tHead.rows[0] && table.tHead.rows[0].cells;
         if (!tableHead) {
             return;
         }
 
-        let i = tableHead.length;
-        const ref = this;
-        while (--i >= 0) {
+        Array.from(tableHead).forEach((cell, i) =>
             (i => {
                 let dir = 1;
-                tableHead[i].addEventListener('click', () => ref.sortTable(table, i, ++dir % 2 === 0));
-            })(i);
-        }
+                cell.addEventListener('click', () => this.sortTable(table, i, ++dir % 2 === 0));
+            })(i));
     }
 
     createTableHeader(entityObject) {
@@ -60,13 +54,14 @@ export default class EntityTable extends React.Component {
             <thead>
             <tr>
                 <th>Id</th>
-                {Object.keys(entityObject).map(key => <th key={entityObject[key].value}>{entityObject[key].value}</th>)}
+                {Object.keys(entityObject).map(key =>
+                    <th key={entityObject[key].value}>{entityObject[key].value}</th>)}
             </tr>
             </thead>
         );
     }
 
-    getTableContent(key, value) {
+    createTableColumn(key, value) {
         return (
             <td key={key + "_" + value}>
                 {key === '_id' ? <Link to={this.props.apipath + '/' + value}>{value}</Link> : value}
@@ -77,65 +72,21 @@ export default class EntityTable extends React.Component {
     createTableRow(entity) {
         return (
             <tr key={entity._id}>
-                {Object.keys(entity).map(key => this.getTableContent(key, entity[key], entity))}
+                {Object.keys(entity).map(key => this.createTableColumn(key, entity[key], entity))}
             </tr>
         );
     }
 
-    formatDate(timestamp) {
-        const date = new Date(timestamp);
-        return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-    }
-
-    formatEntities(entities) {
-        if (!entities) {
-            return;
-        }
-
-        const entityObject = FormEntities.getEntityObject(this.props.apipath);
-        const result = [];
-
-        entities.forEach((entity, index) => {
-            result[index] = {_id: entity._id};
-            Object.keys(entityObject).forEach(key => {
-                result[index][key] = this.processTypedEntity(entityObject[key].type, entity[key], entityObject[key]);
-            });
-        });
-
-        return result;
-    }
-
-    processTypedEntity(type, entity, entityObject) {
-        switch (type) {
-        case "date":
-            return this.formatDate(entity);
-        case "entity_reference":
-            return this.createForeignEntityLink(entityObject.identifiers, entityObject.endpoint, entity);
-        default:
-            return entity;
-        }
-    }
-
-    createForeignEntityLink(identifierFields, endpoint, entity) {
-        const sanity = identifierFields && endpoint && entity;
-        if (!sanity) {
-            return;
-        }
-
-        return <Link to={endpoint + "/" + entity._id}>{identifierFields.map(field => entity[field] + " ")}</Link>;
-    }
-
-    getTableContents(entities) {
-        const formatEntities = this.formatEntities(entities);
-        return <tbody>{formatEntities.map(entity => this.createTableRow(entity))}</tbody>;
+    createTableContents(entities) {
+        return <tbody>{entities.map(entity => this.createTableRow(entity))}</tbody>;
     }
 
     componentDidUpdate(prevProps) {
         if (this.prevPath) {
             this.prevPath = null;
             this.lastCellSorted = -1;
-            const entityTable = document.getElementById("entityTable");
-            this.makeSortable(entityTable);
+
+            this.makeSortable(document.getElementById("entityTable"));
 
             const searchInput = document.getElementById("tableSearch");
             if (searchInput) {
@@ -147,6 +98,9 @@ export default class EntityTable extends React.Component {
     }
 
     render() {
+        const entityObject = FormEntities.getEntityObject(this.props.apipath);
+        const formattedEntities = EntityFormatter.formatEntities(this.props.entities, entityObject);
+
         return (
             <div className="carEvaluationInfoContain">
                 <div className="containerHeading">
@@ -158,7 +112,7 @@ export default class EntityTable extends React.Component {
                             <div className="col-xs-6">
                                 <div className="row text-right">
                                     <div className="col-xs-offset-6 col-xs-4">
-                                        <TableSearchInput placeholder="Søk" id="tableSearch" tableId={"entityTable"} />
+                                        <TableSearchInput placeholder="Søk" id="tableSearch" tableId={"entityTable"}/>
                                     </div>
                                     <div className="col-xs-2">
                                         <Link className="btn btn-primary form-control" to={this.props.apipath + '/new'}>
@@ -172,8 +126,8 @@ export default class EntityTable extends React.Component {
                 </div>
                 <div className="carEvaluationInfo">
                     <table className="table" id="entityTable">
-                        {this.createTableHeader(FormEntities.getEntityObject(this.props.apipath))}
-                        {this.getTableContents(this.props.entities)}
+                        {this.createTableHeader(entityObject)}
+                        {this.createTableContents(formattedEntities)}
                     </table>
                 </div>
             </div>
