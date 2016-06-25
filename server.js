051dbe756dcdf06d5dcd987ff2111514b6a094fe
Jonas Jensen <jonas.jensen@msn.com>
2016-06-25 18:33:44 +0200

    extracted csv logic into separate module

--- server.js
+++ server.js
@@ -30,42 +30,8 @@ const entityManager = require('./routes/entityManager');
 // TODO get these from FormEntities -> single source of truth!
 ['Mailbox', 'Tenant', 'Lease', 'Room', 'Invoice'].forEach(entity => entityManager.setupEntity(app, 'api', entity));
 
-
-function generateCsvLine(lease, delimiter) {
-    const date = new Date();
-    const year = date.getFullYear();
-    const currentDate = date.getDate() + '.' + date.getMonth() + '.' + String(year).slice(2, 4);
-
-    const art = 1;
-    const dato = currentDate;
-    const bilag = 0; // fakturanummer
-    const mva = 9;
-    const debetkonto = 0; // kundenummer
-    const kreditkonto = "";
-    const beloep = lease._room && lease._room.rent;
-
-    const ordered = [art, dato, bilag, mva, debetkonto, kreditkonto, beloep];
-    return ordered.join(delimiter) + "\n";
-}
-
-app.use("/api/makecsv", (req, res) => {
-    const fetch = require('node-fetch');
-    fetch.Promise = require('bluebird');
-    const fs = require("fs");
-
-    fetch("http://localhost:3000/api/leases")
-        .then(res => res.json())
-        .then(leases => {
-            const delimiter = ",";
-            const headerColumns = ["Art", "Dato", "Bilag", "Mva", "debetkonto", "kreditkonto", "Beløp"];
-            const headerString = headerColumns.join(delimiter) + "\n";
-            const csvString = headerString + leases.map(lease => generateCsvLine(lease, delimiter)).join('');
-            
-            res.set('Content-Type', 'text/csv');
-            res.set('Content-Disposition', 'attachment;filename=Invoice.csv');
-            res.send(csvString);
-        });
-});
+const makeCsv = require('./routes/makeCsv');
+app.use("/api/makecsv", (req, res) => makeCsv.serveCsv(res));
 
 const indexHtmlPath = path.join(__dirname, 'public', 'index.html');
 app.use((req, res) => res.sendFile(indexHtmlPath));
