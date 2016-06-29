20f236bc5a86fe0aae032481355be6f7230dca79
Jonas Jensen <jonas.jensen@msn.com>
2016-06-29 15:54:34 +0200

    add ssn attribute to mongoose model

--- models/Tenant.js
+++ models/Tenant.js
@@ -1,12 +1,16 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var tenantSchema = mongoose.Schema({
+const tenantSchema = mongoose.Schema({
+    ssn: Number,
     firstName: String,
     middleName: String,
     lastName: String,
     email: String,
     phone: String,
-    _mailbox: {type: mongoose.Schema.Types.ObjectId, ref: 'Mailbox'}
+    _mailbox: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Mailbox'
+    }
 });
 
 module.exports = mongoose.model('Tenant', tenantSchema);
