7968be5c5b5c35e31707762d978b809f3b1d7df0
Jonas Jensen <jonas.jensen@msn.com>
2016-07-13 16:46:38 +0200

    [WIP] init work

--- public/routes/components/ContentTable/index.js
+++ public/routes/components/ContentTable/index.js
@@ -6,6 +6,39 @@ import EntityFormatter from "./EntityFormatter";
 import EntityTable from "./EntityTable";
 
 export default class ContentTable extends Component {
+    showUnassociatedEntities() {
+        // new branch!
+        // clear search field query first?
+
+        // iterate trhough entities, see if they are associated with entities found in given /apipath (leases)
+        console.log(this.props.entities);
+        /*
+        const searchText = event.target.value.toLowerCase();
+        const entityTable = document.getElementById(this.props.tableId);
+        const rows = Array.from(entityTable.tBodies[0].rows);
+
+        if (searchText === "") {
+            rows.forEach(row => {
+                row.style.display = "table-row";
+            });
+            return;
+        }
+
+        rows.forEach(row => {
+            const cellCount = row.cells.length;
+            for (let j = 0; j < cellCount; j++) {
+                if (row.cells[j].textContent.toLowerCase().includes(searchText)) {
+                    row.style.display = "table-row";
+                    break;
+                }
+
+                if (j === cellCount - 1) {
+                    row.style.display = "none";
+                }
+            }
+        });*/
+    }
+
     render() {
         const apiPath = this.props.apipath;
         const entityObject = ApplicationEntities.getEntityObject(apiPath);
@@ -22,7 +55,18 @@ export default class ContentTable extends Component {
                             <div className="col-xs-6">
                                 <div className="row text-right">
                                     <div className="col-xs-offset-6 col-xs-4">
-                                        <TableSearchInput placeholder="Søk" id="tableSearch" tableId={"entityTable"}/>
+                                        <div className="row">
+                                            <div className="col-xs-4">
+                                                <button className="btn btn-default"
+                                                        onClick={() => this.showUnassociatedEntities()}>
+                                                    Ledige
+                                                </button>
+                                            </div>
+                                            <div className="col-xs-8">
+                                                <TableSearchInput placeholder="Søk" id="tableSearch"
+                                                                  tableId={"entityTable"}/>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div className="col-xs-2">
                                         <Link className="btn btn-primary form-control" to={apiPath + '/new'}>
